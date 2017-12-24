58489f015ce2de3c0259b7ae8e75bcb99fbac619
Saurav <irazer19@gmail.com>
2017-12-24 13:30:36 +0530

    changed sqlite3 database destination

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 
 SECRET_KEY = "Super_secret_key"
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:////vagrant/food-zone/restaurant.db'  # NOQA
+SQLALCHEMY_DATABASE_URI = 'sqlite:////restaurant.db'  # NOQA
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 UPLOADED_IMAGES_DEST = '/vagrant/restaurant/static/images'
 UPLOADED_IMAGES_URL = '/static/images/'
