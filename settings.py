ecab169dd781630bfcfd84b5499845d91c322768
Saurav <irazer19@gmail.com>
2017-12-26 12:20:48 +0530

    changed the database path

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 
 SECRET_KEY = "Super_secret_key"
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:////restaurant.db'  # NOQA
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vagrant:vagrant@0.0.0.0/restaurant'  # NOQA
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 UPLOADED_IMAGES_DEST = '/vagrant/restaurant/static/images'
 UPLOADED_IMAGES_URL = '/static/images/'
